1dcc091b377a61b5ac3f8da4451055be5a7afe1b
Ivanstein <ivanstein18@yandex.ru>
2022-03-17 13:58:52 +0300

    lesson

---
+++ server_uri.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+from fastapi import Body, Form
+
+
+app = FastAPI()
+
+
+@app.get("/unify_phone_from_form")
+def page(request: Request):
+
+    header = request.headers.get('path')
+
+    phone = header.split('?')[-1]
+    phone_list = phone.split("=")
+    phone_namber = phone_list[-1]
+
+
+    phone_namber = list(phone_namber.strip())
+    pn_modern = []
+
+    for i in phone_namber:
+        if i in '1234567890':
+            pn_modern.append(i)
+
+    if (pn_modern[0] == '7' or pn_modern[0] == '8') and pn_modern[1] == '9' and len(pn_modern) == 11:
+        phone_n = f"8 ({''.join(pn_modern[1:4])}) {''.join(pn_modern[4:7])}-{''.join(pn_modern[7:9])}-{''.join(pn_modern[9:11])}"
+
+    elif pn_modern[0] == '9' and len(pn_modern) == 10:
+        phone_n = f"8 ({''.join(pn_modern[0:3])}) {''.join(pn_modern[3:6])}-{''.join(pn_modern[6:8])}-{''.join(pn_modern[8:10])}"
+
+    else:
+        phone_n = ''.join(pn_modern)
+
+    return Response(phone_n, media_type= "text/html")
